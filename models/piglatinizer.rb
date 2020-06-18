b6d248a7459bbd6a20a5b874ee9ede61d271afb9
Parker Williamson <parker.williamson@gmail.com>
2020-06-18 18:53:28 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,28 +1,23 @@
 class PigLatinizer
-
-  def piglatinize(user_phrase)
-      @user_phrase = user_phrase
-      string = @user_phrase
-      words = string.split(" ")
-      phrase = words.collect  {|word| piglatinize_word(word)}
-      phrase.join(" ")
-  end
-  
-  def piglatinize_word(word)
-      first_letter = word[0].downcase
-  
-      if ["a", "e", "i", "o", "u"].include?(first_letter)
-        "#{word}way"
-      else
-        consonants = []
-        consonants << word[0]
-          if ["a", "e", "i", "o", "u"].include?(word[1]) == false
-            consonants << word[1]
-            if ["a", "e", "i", "o", "u"].include?(word[2]) == false
-              consonants << word[2]
+    def piglatinize(string)
+        split_str = string.split(' ')
+        pig_str = split_str.map {|word| piglatinize_word(word)}
+        pig_str.join(' ')
+    end
+    def piglatinize_word(word)
+        first_letter = word[0].downcase
+        if ["a", "e", "i", "o", "u"].include?(first_letter)
+            "#{word}way"
+        else
+            consonants = []
+            consonants << word[0]
+              if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+                consonants << word[1]
+                if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+                  consonants << word[2]
+                end
             end
-          end
-        "#{word[consonants.length..-1] + consonants.join + "ay"}"
-      end
-  end
+            "#{word.slice(consonants.length , word.length)}#{consonants.join}ay"
+        end
+    end
 end
