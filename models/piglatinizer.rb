1bdc77708f0dee4ceda41404c7f86aaa11cbc060
Parker Williamson <parker.williamson@gmail.com>
2020-06-18 18:30:21 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,29 @@
 class PigLatinizer
+
+  def piglatinize(user_phrase)
+      @user_phrase = user_phrase
+      string = @user_phrase
+      words = string.split(" ")
+      phrase = words.collect  {|word| piglatinize_word(word)}
+      phrase.join(" ")
+  end
   
-end
+  def piglatinize_word(word)
+      first_letter = word[0].downcase
+  
+      if ["a", "e", "i", "o", "u"].include?(first_letter)
+        # piglatinize word that starts with a vowel
+        "#{word}way"
+      else
+        consonants = []
+        consonants << word[0]
+          if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+            consonants << word[1]
+            if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+              consonants << word[2]
+            end
+          end
+        # piglatinize word that starts with a consonant
+        "#{word[consonants.length..-1] + consonants.join + "ay"}"
+      end
+  end
